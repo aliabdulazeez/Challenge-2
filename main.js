7a42303a9172c66ce116aed73567aced64ac7dda
aliabdulazeez <aliabdulazeez01@gmail.com>
2020-09-19 20:10:33 +0900

    desctop ready

--- main.js
+++ main.js
@@ -1,42 +1,16 @@
 
-const parent = document.getElementById('here').lastElementChild;
+const w = document.getElementById("davron");
 
+const button = document.getElementById('share');
 
-const button = document.getElementById('share').addEventListener('click', () => {
+button.addEventListener('click', () => {
 
-    
-    const has = document.getElementById('here').innerHTML;
+    if(w.classList[1] == "disabled"){
 
-     console.log(has);
+        w.classList.remove("disabled");
 
-        add();
+    }else{
 
-     console.log(has);
-    
-
-   
-    
-      
-});
-
-
-function add(){
-    const shares = document.createElement('div');
-
-    shares.className = 'shares';
-
-    shares.innerHTML = `
-        <h3>SHARE</h3>
-        <img src="images/icon-facebook.svg" alt="">
-        <img src="images/icon-pinterest.svg" alt="">
-        <img src="images/icon-twitter.svg" alt="">
-    `;
-
-    document.getElementById('here').appendChild(shares);
-}
-
-
-function remove() {
-    // const shares = document.getElementsByClassName('shares');
-    document.getElementById('here').removeChild(shares);
-  }
+        w.classList.add("disabled");
+    }  
+})
